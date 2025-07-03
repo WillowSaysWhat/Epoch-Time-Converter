2d73c5812aada22d7e6d47de914ac43422b5a16a
Huw <126318401+WillowSaysWhat@users.noreply.github.com>
2025-07-03 14:02:01 +0100

    further development of 2.1
    images added.
    panel is almost complete.
    ready for animations.

--- EpochTimeConverter/EpochTimeConverter/Model/Card.swift
+++ EpochTimeConverter/EpochTimeConverter/Model/Card.swift
@@ -13,16 +13,17 @@ struct Card: Identifiable {
     var title: String
     var description: String
     var color: Color
+    var image: Image
     
 }
 
 var cards: [Card] = [
-    Card(tag: "relative", title: "Relative Time", description: "Guys, the mission will start approx ", color: .indigo),
-    Card(tag: "default", title: "Day & Date", description: "We have organised a mission for ", color: .red),
-    Card(tag: "short", title: "Short Date", description: "We have organised a mission for the ", color: .blue),
-    Card(tag: "long", title: "Long Date", description: "We have organised a mission for the ", color: .green),
-    Card(tag: "timeLong", title: "Long Time", description: "The mission will be kicking off at ", color: .brown),
-    Card(tag: "timeShort", title: "Short Time", description: "We have organised a mission for the ", color: .orange),
-    Card(tag: "shortDateTime", title: "Short Date & Time", description: "We have organised a mission for the ", color: .pink),
-    Card(tag: "longDateTime", title: "Long Date & Time", description: "We have organised a mission for ", color: .teal)
+    Card(tag: "relative", title: "Relative Time", description: "Guys, the mission will start approx in 3 hours", color: .indigo, image: Image(.relative)),
+    Card(tag: "default", title: "Day & Date", description: "We have organised a mission for ", color: .red, image: Image(.default)),
+    Card(tag: "short", title: "Short Date", description: "We have organised a mission for the ", color: .blue, image: Image(.shortdate)),
+    Card(tag: "long", title: "Long Date", description: "We have organised a mission for the ", color: .green, image: Image(.longDate)),
+    Card(tag: "timeLong", title: "Long Time", description: "The mission will be kicking off at ", color: .brown, image: Image(.longTime)),
+    Card(tag: "timeShort", title: "Short Time", description: "We have organised a mission for the ", color: .orange, image: Image(.shortTime)),
+    Card(tag: "shortDateTime", title: "Short Date & Time", description: "We have organised a mission for the ", color: .pink, image: Image(.shortDateTime)),
+    Card(tag: "longDateTime", title: "Long Date & Time", description: "We have organised a mission for ", color: .teal, image: Image(.longdateTime))
     ]
--- EpochTimeConverter/EpochTimeConverter/View/CardView.swift
+++ EpochTimeConverter/EpochTimeConverter/View/CardView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct CardView: View {
-    var card: Card = cards[0]
+    var card: Card = cards[1]
+    @EnvironmentObject var controller: HomeViewController
+    @State var textFieldOutput: String = "<t:000000000:f>"
     
     // @Binding var screenSize: CGSize
     @State var isTapped: Bool = false
@@ -16,22 +18,156 @@ struct CardView: View {
     @State var time = Date.now
     
     var body: some View {
+        
         ZStack {
-            RoundedRectangle(cornerRadius: isTapped ? 0 : 20)
-                .fill(card.color.gradient)
-                .frame(width: isTapped ? 393 : 360, height: isTapped ? 600 : 500)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 20)
-                        .blendMode(.softLight)
+            card.image
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(height: 350)
+                .cornerRadius(20)
+            
+            VStack(alignment: .center) {
+                
+                
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        // profile image
+                        Image("brand")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30)
+                            .clipShape(Circle())
                         
-                )
-                .cornerRadius(isTapped ? 0 : 20)
-                .overlay(
-                    Text(card.title)
-                        .font(.system(size: isTapped ? 80 : 17))
-                        .foregroundStyle(card.color)
-                        .fontWeight(isTapped ? .bold : .semibold)
-                    )
+                        Text("Unknown User")
+                            .font(.title2)
+                            .fontWeight(.black)
+                            .foregroundStyle(card.color)
+                        
+                        Image(systemName: "crown.fill")
+                            .foregroundStyle(card.color)
+                    }
+                    
+                    Text(card.description)
+                        .font(.subheadline)
+                }
+                
+                
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Example")
+                            .foregroundStyle(.secondary)
+                        
+                        Text("\(controller.setMessage(tag: card.tag))")
+                        
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    
+                    Divider()
+                    
+                    VStack(alignment: .leading) {
+                        Text("Format")
+                            .foregroundStyle(.secondary)
+                        
+                        Text("\(setFormatColumn(tag: card.tag))")
+                        
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    
+                    Divider()
+                    
+                    VStack(alignment: .leading) {
+                        Text("Type")
+                            .foregroundStyle(.secondary)
+                        
+                        Text("\(card.tag)")
+                        
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                }
+                .frame(maxHeight: 44)
+                Divider()
+                HStack {
+                    
+                    DatePicker("", selection: $controller.date)
+                        .datePickerStyle(.compact)
+                        .labelsHidden()
+                        .padding(7)
+                    
+                    Image(systemName: "list.clipboard")
+                        .offset(x: -8)
+                    
+                }
+                .overlay {
+                    RoundedRectangle(cornerRadius: 20)
+                        .strokeBorder(linearGradient)
+                }
+                .frame(maxHeight: 44)
+                
+                
+                Image(systemName: "sparkles")
+                    .imageScale(.large)
+                    .offset(y: 10)
+            }
+            .padding(20)
+            .background(.ultraThinMaterial)
+            .overlay {
+                RoundedRectangle(cornerRadius: 20)
+                    .strokeBorder(linearGradient)
+            }
+            .cornerRadius(20)
+            .padding(20)
+            .offset(y: 80)
+            
+            // play button
+            HStack {
+                Image(systemName: isTapped ? "x.circle" : "play")
+                Image(systemName: "repeat")
+            }
+            .font(.largeTitle)
+            .padding(20)
+            .background(.ultraThinMaterial)
+            .overlay {
+                RoundedRectangle(cornerRadius: 20)
+                    .strokeBorder(linearGradient)
+            }
+            .cornerRadius(20)
+            .offset(y: -44)
+        }
+        .frame(maxWidth: 400)
+        .dynamicTypeSize(.xSmall ... .xLarge)
+        
+    }
+    
+    // gradient on the border of the panel
+    var linearGradient: LinearGradient {
+        LinearGradient(colors: [.clear, .primary.opacity(0.3), .clear], startPoint: .topLeading, endPoint: .bottomTrailing)
+    }
+    func setFormatColumn(tag: String) -> String {
+        
+        switch tag {
+        case "default":
+            return "<t:000000:F>"
+        case "long":
+            return "<t:000000:D>"
+        case "short":
+            return "<t:000000:d>"
+        case "relative":
+            return "<t:000000:R>"
+        case "timeLong":
+            return "<t:000000:T>"
+        case "timeShort":
+            return "<t:000000:t>"
+        case "longDateTime":
+            return "<t:000000:F>"
+        case "shortDateTime":
+            return "<t:000000:f>"
+        default:
+            return "#ERROR"
             
         }
     }
@@ -39,4 +175,7 @@ struct CardView: View {
 
 #Preview {
     CardView()
+        .environmentObject(HomeViewController())
 }
+
+
